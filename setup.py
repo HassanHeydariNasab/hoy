d5e680e3edbab40e447d900b9ec39cc8b76b185f
hsn6 <hsn6@openmailbox.org>
2016-06-26 12:37:29 +0430

    change six version to 1.10.0

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['peewee','telepot', 'BeautifulSoup', 'fuzzywuzzy', 'python-Levenshtein', 'hazm'],
+      install_requires=['peewee','telepot', 'BeautifulSoup', 'fuzzywuzzy', 'python-Levenshtein', 'hazm', 'six==1.10.0'],
      )
